bd552253f9bf572738ceac50033d42645d244766
CHWJ <zhilinger2010@gmail.com>
2018-08-08 23:29:36 +0800

    First commit.

---
+++ src/main/java/com/hdtech/dianligongdan/DianligongdanApplication.java
@@ -0,0 +1,12 @@
+package com.hdtech.dianligongdan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DianligongdanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DianligongdanApplication.class, args);
+    }
+}
---
+++ src/main/java/com/hdtech/dianligongdan/controller/HomeController.java
@@ -0,0 +1,7 @@
+package com.hdtech.dianligongdan.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController extends BaseController {
+}
